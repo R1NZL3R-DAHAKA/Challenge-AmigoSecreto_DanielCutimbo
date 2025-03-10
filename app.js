8984cd9c6baa92a21e96278f3f2516783411a64e
SammaeL <66368599+R1NZL3R-DAHAKA@users.noreply.github.com>
2025-03-09 20:38:40 -0500

    Agregando functions generales - app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function agregarAmigo(){
+    
+}
+
+function listarAmigos(){
+
+}
+
+function verificarAmigo(){
+
+}
+
+function sorteo(){
+
+}
+
