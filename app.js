ad8861b7a4a103d7f5ed823bfb317b6aa2e51804
SammaeL <66368599+R1NZL3R-DAHAKA@users.noreply.github.com>
2025-03-09 21:17:44 -0500

    Funcion agregar amigo - app.js

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-function agregarAmigo(){
+let amigos = [];
+// Función para agregar un amigo a la lista
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (!verificar(nombre)) {
+        return;
+    }
+
+    amigos.push(nombre);
+    input.value = "";
+    listarAmigos();
+}
+
+// Función para verificar si el nombre del amigo es válido
+function verificar(nombre) {
     
 }
 
-function listarAmigos(){
-
+function listarAmigos() {
+    
 }
 
-function verificarAmigo(){
-
+function sortearAmigo() {
+    
 }
 
-function sorteo(){
-
+function mostrarResultado(amigoSecreto) {
+    
 }
-
